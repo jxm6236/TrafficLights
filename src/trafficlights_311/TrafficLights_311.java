b325a0dfaf3b358113d89bc2629e2547e39f386f
jxm6236 <54914673+jxm6236@users.noreply.github.com>
2019-12-02 21:27:33 -0500

    Final Commit

--- src/trafficlights_311/TrafficLights_311.java
+++ src/trafficlights_311/TrafficLights_311.java
@@ -19,7 +19,7 @@ public class TrafficLights_311 extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXML_Lights.fxml"));
         
         Scene scene = new Scene(root);
         
